2af99bd8e9bfcd10e2c2d05ab21a9a5c1bd8228a
blag <blag@users.noreply.github.com>
2021-11-22 23:41:28 -0800

    Refactor build script a bit more for DRY

--- scripts/ci/astronomer-qa-nightly-build.sh
+++ scripts/ci/astronomer-qa-nightly-build.sh
@@ -32,34 +32,49 @@ fi
 
 echo "Building and releasing a QA package"
 echo
-git fetch
+# This output can be very long, and is mostly useless
+git fetch >/dev/null
 
-NEW_VERSION="$(date +%Y%m%d)"
-sed -i -E "s/dev[0-9]+/dev${NEW_VERSION}/g" airflow/version.py
-sed -i -E "s/dev[0-9]+(\+astro)?/dev${NEW_VERSION}/g" setup.py
+DATE_STRING="$(date +%Y%m%d)"
+sed -i -E "s/dev[0-9]+/dev${DATE_STRING}/g" airflow/version.py
+sed -i -E "s/dev[0-9]+(\+astro)?/dev${DATE_STRING}/g" setup.py
 
 UPDATED_AIRFLOW_VERSION=$(awk '/version = /{print $NF}' setup.py | tr -d \')
 export UPDATED_AIRFLOW_VERSION
 echo "Updated Airflow Version: $UPDATED_AIRFLOW_VERSION"
 
-python3 setup.py --quiet compile_assets bdist_wheel --dist-dir dist/apache-airflow/
+AIRFLOW_DIST_DIR="dist/apache-airflow"
 
-ls -altr dist/*/*
+python3 setup.py --quiet compile_assets bdist_wheel --dist-dir "$AIRFLOW_DIST_DIR"
+
+# Get the package name
+# As long as there's only one, this will grab it
+AIRFLOW_PACKAGE_PATH=$(echo $AIRFLOW_DIST_DIR/apache_airflow-*.whl)
+# shellcheck disable=SC2034
+AIRFLOW_PACKAGE_NAME=$(basename "$AIRFLOW_PACKAGE_PATH")
+
+ls -altr $AIRFLOW_DIST_DIR/*
+
+AC_DIST_DIR="dist/astronomer-certified"
 
 # Build the astronomer-certified release from the matching apache-airflow wheel file
-python3 scripts/ci/astronomer-certified-setup.py bdist_wheel --dist-dir dist/astronomer-certified dist/apache-airflow/apache_airflow-*.whl
+python3 scripts/ci/astronomer-certified-setup.py bdist_wheel --dist-dir "$AC_DIST_DIR" "$AIRFLOW_PACKAGE_PATH"
 
-ls -altr dist/astronomer-certified/*
+ls -altr $AC_DIST_DIR/*
 
+# Get the package name
+# As long as there's only one, this will grab it
+AC_PACKAGE_PATH=$(echo $AC_DIST_DIR/astronomer_certified-*.whl)
+AC_PACKAGE_NAME=$(basename "$AC_PACKAGE_PATH")
 # Get the version of AC (Example 1.10.7.post7)
-CURRENT_AC_VERSION=$(echo dist/astronomer-certified/astronomer_certified-*.whl | sed -E 's|.*astronomer_certified-(.+)-py3-none-any.whl|\1|')
+CURRENT_AC_VERSION=$(echo "$AC_PACKAGE_NAME" | sed -E 's|.*astronomer_certified-(.+)-py3-none-any.whl|\1|')
 export CURRENT_AC_VERSION
 echo "AC Version: $CURRENT_AC_VERSION"
 
 # Get the version of Apache Airflow (Example 1.10.7)
-AIRFLOW_BASE_VESION=$(echo "$CURRENT_AC_VERSION" | sed -E 's|([0-9]+\.[0-9]+\.[0-9]+).*|\1|')
-export AIRFLOW_BASE_VESION
-echo "Airflow Base Version: $AIRFLOW_BASE_VESION"
+AIRFLOW_BASE_VERSION=$(echo "$CURRENT_AC_VERSION" | sed -E 's|([0-9]+\.[0-9]+\.[0-9]+).*|\1|')
+export AIRFLOW_BASE_VERSION
+echo "Airflow Base Version: $AIRFLOW_BASE_VERSION"
 
 # Store the latest version info in a separate file
 # Example: 'astronomer-certified/latest-1.10.7.build' contains '1.10.7.post7'
