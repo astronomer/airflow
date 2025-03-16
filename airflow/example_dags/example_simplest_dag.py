d30d8e4a14dd25a41cca52067b7ae8d3f7ef91d9
TJaniF <tamara.fingerlin@astronomer.io>
2025-03-16 18:39:22 +0100

    black lint

--- airflow/example_dags/example_simplest_dag.py
+++ airflow/example_dags/example_simplest_dag.py
@@ -1,11 +1,13 @@
 from airflow.decorators import dag, task
 
-@dag 
-def example_simplest_dag(): 
-    @task 
+
+@dag
+def example_simplest_dag():
+    @task
     def my_task():
         pass
 
     my_task()
+
 
 example_simplest_dag()
