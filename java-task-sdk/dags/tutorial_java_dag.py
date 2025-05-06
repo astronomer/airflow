b03692d0dc2394e968140d582487ad4519c4de28
Vikram Koka <vikram@astronomer.io>
2025-05-06 08:40:17 -0700

    Example Java dag based on tutorial dag
    Example Java Tutorial DAG which is based on the example tutorial DAG
    - the key reason to try this out is to demonstrate and validate Xcom data passing between tasks

---
+++ java-task-sdk/dags/tutorial_java_dag.py
@@ -0,0 +1,21 @@
+import os
+
+from airflow.sdk import DAG, task
+
+with DAG(dag_id="tutorial_java_dag"):
+    
+    # Extract
+    #
+    task.remote(executor="EdgeExecutorJava", queue="java", task_id="extract") 
+
+
+    # Transform 
+    #
+    task.remote(executor="EdgeExecutorJava", queue="java", task_id="transform") 
+
+    # Load
+    #
+    task(executor="EdgeExecutorJava", queue="java", task_id="load") 
+
+
+
