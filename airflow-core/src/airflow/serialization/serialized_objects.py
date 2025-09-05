4e4271c3c7ed00063bd606c2e69a831925cc0f2b
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2025-09-05 08:06:50 -0600

    Remove python_callable as string from mapped operator in serialized Dag (#55288)
    Remove string serialization of python_callable in mapped tasks partial_kwargs.
    This reduces the serialized Dag size.
    
    (cherry picked from commit 84c55eed9804071619dcb9195b74dc55d1cf7453)

--- airflow-core/src/airflow/serialization/serialized_objects.py
+++ airflow-core/src/airflow/serialization/serialized_objects.py
@@ -1311,6 +1311,13 @@ class SerializedBaseOperator(BaseOperator, BaseSerialization):
             if v == default:
                 del serialized_partial[k]
 
+        # we want to store python_callable_name, not python_callable
+        python_callable = op.partial_kwargs.get("python_callable", None)
+        if python_callable:
+            callable_name = qualname(python_callable)
+            serialized_op["partial_kwargs"]["python_callable_name"] = callable_name
+            del serialized_op["partial_kwargs"]["python_callable"]
+
         serialized_op["_is_mapped"] = True
         return serialized_op
 
