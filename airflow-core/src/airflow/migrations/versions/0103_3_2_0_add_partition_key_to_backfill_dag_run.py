df3c81cab34e3b7199493507dbab3fa4f1e03ab4
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2026-02-12 07:42:48 -0800

    fix migration

--- airflow-core/src/airflow/migrations/versions/0103_3_2_0_add_partition_key_to_backfill_dag_run.py
+++ airflow-core/src/airflow/migrations/versions/0103_3_2_0_add_partition_key_to_backfill_dag_run.py
@@ -42,13 +42,14 @@ airflow_version = "3.2.0"
 
 def upgrade():
     """Apply Add partition_key to backfill_dag_run."""
-    op.add_column("dag_run", sa.Column("created_at", UtcDateTime, nullable=True))
+    op.add_column("dag_run", sa.Column("created_at", UtcDateTime(timezone=True), nullable=True))
     op.execute("update dag_run set created_at = run_after;")
+    with op.batch_alter_table("dag_run", schema=None) as batch_op:
+        batch_op.alter_column("created_at", existing_type=UtcDateTime(timezone=True), nullable=False)
 
     with op.batch_alter_table("backfill_dag_run", schema=None) as batch_op:
         batch_op.add_column(sa.Column("partition_key", StringID(), nullable=True))
         batch_op.alter_column("logical_date", existing_type=sa.TIMESTAMP(), nullable=True)
-        batch_op.alter_column("created_at", existing_type=sa.TIMESTAMP(), nullable=False)
 
 
 def downgrade():
